3a54f26a8a2035c1b435c56880d5907dafe9d0ba
Tsvetan Ganev <tsvetan.ganev@hotmail.com>
2018-06-05 01:16:21 +0300

    [tslint] Fixes errors

--- src/app/models/professor.model.ts
+++ src/app/models/professor.model.ts
@@ -1,17 +1,17 @@
 import { UserRole } from './../models/authentication-models';
 
-export interface IProfessor extends INewProfessor {
-  id: number;
-  role: UserRole;
-  active: boolean;
-  subjectsTeaching?: ISubject[];
-}
-
 export interface INewProfessor {
   username: string;
   fullName: string;
   cabinet: string;
   phoneNumber: string;
+}
+
+export interface IProfessor extends INewProfessor {
+  id: number;
+  role: UserRole;
+  active: boolean;
+  subjectsTeaching?: ISubject[];
 }
 
 export interface ISubject {
